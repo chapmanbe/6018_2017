c5c9da59d81c4a736100bdf0474849169891f49f
Brian Chapman <brian.chapman@utah.edu>
2017-10-04 13:28:47 -0600

    added m8 in-class materials

---
+++ modules/m8-modules/InClass/00-first.py
@@ -0,0 +1,2 @@
+import sys
+sys.path.append("/home/jovyan/work/6018_2017/modules/m8-modules/ClassPrep")
---
+++ modules/m8-modules/InClass/ziggy.py
@@ -0,0 +1,6 @@
+#! /opt/conda/bin/python
+
+print("""Ziggy played guitar, jamming good with Weird and Gilly
+And the spiders from Mars. He played it left hand
+But made it too far
+Became the special man, then we were Ziggy's band""")
