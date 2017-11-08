b899f19c839f192bc64ce92dc3df49e6918aab24
Brian Chapman <brian.chapman@utah.edu>
2017-11-07 18:31:19 -0700

    updated inclass.py

--- modules/m15_databases/InClass/inclass.py
+++ modules/m15_databases/InClass/inclass.py
@@ -1,5 +1,9 @@
 all_rad_query = """SELECT * FROM  noteevents WHERE category= 'RADIOLOGY_REPORT'"""
 
+icd9_416_0_query = """SELECT noteevents.*, icd9.code  FROM noteevents INNER JOIN icd9 ON 
+                      noteevents.subject_id = icd9.subject_id 
+               WHERE icd9.code ='416.0'"""
+
 pah_query = """SELECT noteevents.subject_id, 
                       noteevents.category, 
                       noteevents.text, 
